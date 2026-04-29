a455de289056150e3c79a0edf50e96c9216878b7
GeorgeMarkas <georgemarkanton@proton.me>
2026-04-29 18:45:10 +0300

    Finalized, probably

--- arena.h
+++ arena.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <stdint.h>
-#include <stdbool.h>
 
 #ifdef ARENA_STATIC
 #define ARENA_DEF static
@@ -9,41 +8,82 @@
 #define ARENA_DEF extern
 #endif
 
-typedef struct {
+typedef struct Arena_Region {
+    struct Arena_Region *next;
     size_t size;
     size_t index;
-    size_t prev_index;
     uintptr_t data[];
+} Arena_Region;
+
+typedef struct {
+    Arena_Region *first;
+    Arena_Region *last;
+    size_t prev_index;
 } Arena;
 
+/**
+ * Allocate a new arena of specified size.
+ * @param size The size of the arena in bytes.
+ * @return Pointer to the arena, or NULL on failure.
+ */
 ARENA_DEF Arena *arena_create(size_t size);
 
+/**
+ * Return a pointer to an arena memory block of specified size.
+ * @param arena The arena whose memory to get a pointer to.
+ * @param size The size of the memory block in bytes.
+ * @return Pointer to arena memory on success, or NULL on failure.
+ */
 ARENA_DEF void *arena_alloc(Arena *arena, size_t size);
 
+/**
+ * Change the size of the arena memory block pointed to by a given pointer.
+ * @param arena The arena the memory block belongs to.
+ * @param old_ptr The pointer to the memory block.
+ * @param old_size The old size of the memory block.
+ * @param new_size The new size of the memory block.
+ * @return A pointer to the resized memory block, or NULL on failure.
+ */
 ARENA_DEF void *arena_realloc(Arena *arena, void *old_ptr, size_t old_size,
                               size_t new_size);
 
-ARENA_DEF bool arena_resize(Arena **arena, size_t new_size);
-
+/**
+ * Free the memory allocated for an arena.
+ * @param arena The arena to be destroyed.
+ */
 ARENA_DEF void arena_destroy(Arena *arena);
 
 #ifdef ARENA_IMPLEMENTATION
 
 #include <stdlib.h>
+#include <string.h>
 
 #define ALIGN sizeof(uintptr_t)
 #define ROUND_UP(size) ((size + (ALIGN - 1)) & ~(ALIGN - 1))
 
-ARENA_DEF Arena *arena_create(const size_t size) {
+static Arena_Region *region_alloc(const size_t size) {
+    const size_t region_size = ROUND_UP(size);
+    Arena_Region *region = malloc(sizeof(Arena_Region) + region_size);
+    if (!region) return NULL;
+
+    region->next = NULL;
+    region->size = region_size;
+    region->index = 0;
+
+    return region;
+}
+
+ARENA_DEF Arena *arena_create(size_t size) {
     if (size == 0) return NULL;
 
-    const size_t alloc_size = ROUND_UP(size);
-    Arena *arena = malloc(sizeof(Arena) + alloc_size);
-    if (arena == NULL) return NULL;
+    Arena *arena = malloc(sizeof(Arena));
+    if (!arena) return NULL;
 
-    arena->size = alloc_size;
-    arena->index = 0;
-    arena->prev_index = 0;
+    Arena_Region *region = region_alloc(size);
+    if (!region) return NULL;
+
+    arena->first = region;
+    arena->last = region;
 
     return arena;
 }
@@ -52,11 +92,18 @@ ARENA_DEF void *arena_alloc(Arena *arena, const size_t size) {
     if (size == 0) return NULL;
 
     const size_t alloc_size = ROUND_UP(size);
-    if (arena->index + alloc_size > arena->size) return NULL;
+    Arena_Region *current = arena->last;
+    if (current->index + alloc_size > current->size) {
+        Arena_Region *region = region_alloc(alloc_size);
+        if (!region) return NULL;
 
-    void *ptr = &arena->data[arena->index];
-    arena->prev_index = arena->index;
-    arena->index += alloc_size;
+        current->next = region;
+        arena->last = current = region;
+    }
+
+    void *ptr = &current->data[current->index];
+    arena->prev_index = current->index;
+    current->index += alloc_size;
 
     return ptr;
 }
@@ -67,32 +114,32 @@ ARENA_DEF void *arena_realloc(Arena *arena, void *old_ptr,
 
     if (!old_ptr || old_size == 0 || !arena) return NULL;
     if (new_size <= old_size) return old_ptr;
-    // Ensure the given pointer is within arena bounds
-    if (arena->data <= (uintptr_t *) old_ptr &&
-        (uintptr_t *) old_ptr < arena->data + arena->size) {
-        // Check if the pointer is the result of the last allocation,
-        // in which case we can just increment / decrement the buffer offset.
-        if (arena->data + arena->prev_index == old_ptr) {
-            const size_t realloc_size = ROUND_UP(new_size);
-            if (arena->prev_index + realloc_size > arena->size) return NULL;
-            arena->index = arena->prev_index + realloc_size;
+    // Check if the pointer is the result of the last allocation,
+    // in which case we can just increment the buffer offset.
+    if (arena->last->data + arena->prev_index == old_ptr) {
+        const size_t realloc_size = ROUND_UP(new_size);
+        if (arena->prev_index + realloc_size <= arena->last->size) {
+            arena->last->index = arena->prev_index + realloc_size;
             return old_ptr;
-        } else {
-            void *new_ptr = arena_alloc(arena, new_size);
-            memmove(new_ptr, old_ptr, old_size);
-            return new_ptr;
         }
     }
 
-    return NULL;
-}
+    void *new_ptr = arena_alloc(arena, new_size);
+    memcpy(new_ptr, old_ptr, old_size);
 
-ARENA_DEF bool arena_resize(Arena **arena, size_t new_size) {
-    // TODO: Implement
+    return new_ptr;
 }
 
 ARENA_DEF void arena_destroy(Arena *arena) {
-    if (!arena) return;
+    Arena_Region *current = arena->first;
+    while (current) {
+        Arena_Region *temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    arena->first = NULL;
+    arena->last = NULL;
     free(arena);
     arena = NULL;
 }
